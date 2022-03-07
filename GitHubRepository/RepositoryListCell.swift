54e9074e3a736f096f9df090a9bf7a610fc72f3d
min-kyoung <shalsrud9@naver.com>
2022-03-07 15:06:08 +0900

    Add : GitHub API 연결하기

--- GitHubRepository/RepositoryListCell.swift
+++ GitHubRepository/RepositoryListCell.swift
@@ -9,11 +9,11 @@ import SnapKit
 import UIKit
 
 class RepositoryListCell: UITableViewCell {
-    var repository: String?
+    var repository: Repository?
     
     let nameLabel = UILabel()
     let descriptionLabel = UILabel()
-    let startImageView = UIImageView()
+    let starImageView = UIImageView()
     let starLabel = UILabel()
     let languageLabel = UILabel()
     
@@ -22,11 +22,57 @@ class RepositoryListCell: UITableViewCell {
         
         [
             nameLabel, descriptionLabel,
-            startImageView, starLabel, languageLabel
+            starImageView, starLabel, languageLabel
         ].forEach {
             contentView.addSubview($0)
         }
         
+        guard let repository = repository else {
+            return
+        }
+        
+        nameLabel.text = repository.name
+        nameLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        
+        descriptionLabel.text = repository.description
+        descriptionLabel.font = .systemFont(ofSize: 15)
+        descriptionLabel.numberOfLines = 2
+        
+        starImageView.image = UIImage(systemName: "star")
+        
+        starLabel.text = "\(repository.stargazersCount)"
+        starLabel.font = .systemFont(ofSize: 16)
+        starLabel.textColor = .gray
+        
+        languageLabel.text = repository.language
+        languageLabel.font = .systemFont(ofSize: 16)
+        languageLabel.textColor = .gray
+        
+        nameLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(18)
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(nameLabel.snp.bottom).offset(3)
+            $0.leading.trailing.equalTo(nameLabel)
+        }
+        
+        starImageView.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(descriptionLabel)
+            $0.width.height.equalTo(20)
+            $0.bottom.equalToSuperview().inset(18)
+        }
+        
+        starLabel.snp.makeConstraints {
+            $0.centerY.equalTo(starImageView)
+            $0.leading.equalTo(starImageView.snp.trailing).offset(5)
+        }
+        
+        languageLabel.snp.makeConstraints {
+            $0.centerY.equalTo(starLabel)
+            $0.leading.equalTo(starLabel.snp.trailing).offset(12)
+        }
         
         
     }
--- GitHubRepository/RepositoryListViewController.swift
+++ GitHubRepository/RepositoryListViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RepositoryListViewController: UITableViewController {
     private let organization = "Apple"
+    private let repositories = BehaviorSubject<[Repository]>(value: [])
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,18 +34,89 @@ class RepositoryListViewController: UITableViewController {
     
     @objc func refresh() {
         // API 네트워킹
+        DispatchQueue.global(qos: .background).async {[weak self] in
+            guard let self = self else { return }
+            self.fetchRepositories(of: self.organization)
+        }
+    }
+    
+    // 네트워크 통신을 해서 json을 가지고 와 디코딩하고, 위에서 생성한 subject에 onNext해주는 함수
+    func fetchRepositories(of organization: String) {
+        Observable.from([organization]) // 함수에 넣을 "Apple"을 array 형태로 받을 from 사용
+            .map { organization -> URL in
+                return URL(string: "https://api.github.com/orgs/\(organization)/repos")! // URL 형태로 변환
+            }
+            .map { url -> URLRequest in
+                var request = URLRequest(url: url) // URLRequest로 변환
+                request.httpMethod = "GET"
+                return request
+            }
+            .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+                return URLSession.shared.rx.response(request: request) // response를 rx로 변환
+                // => response는 URLResponse를 받아서 방금 선언한 URLResponse와 data의 튜플을 가지는 observable sequence로 반환
+            }
+            .filter { responds, _ in
+                return 200..<300 ~= responds.statusCode // 정상적인 응답일 경우에만 넘겨줌
+            }
+            .map { _, data -> [[String: Any]] in
+                guard let json = try? JSONSerialization.jsonObject(with: data, options: []),
+                      let result = json as? [[String: Any]] else {
+                          return []
+                }
+                return result
+            }
+            .filter { result in
+                result.count > 0 // 빈 array는 무시하겠다는 뜻
+            }
+            .map { objects in
+                return objects.compactMap { dic -> Repository? in // dictionary 형태를 repositoty 형태로 바꿈
+                    // compactMap을 사용하면 자동적으로 nil 값은 제거된다.
+                    guard let id = dic["id"] as? Int,
+                          let name = dic["name"] as? String,
+                          let description = dic["description"] as? String,
+                          let stargazersCount = dic["stargazers_count"] as? Int,
+                          let language = dic["language"] as? String else {
+                        return nil
+                    }
+                    
+                    return Repository(id: id, name: name, description: description, stargazersCount: stargazersCount, language: language)
+                }
+            }
+            .subscribe(onNext: {[weak self] newRepositories in
+                self?.repositories.onNext(newRepositories)
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                    self?.refreshControl?.endRefreshing()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
 
 // UITableView DataSourcr Delegate
 extension RepositoryListViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        do {
+            return try repositories.value().count
+        } catch {
+            return 0 // 에러가 발생해서 value 값을 뽑아낼 수 없는 경우
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryListCell", for: indexPath) as? RepositoryListCell
             else { return UITableViewCell()}
+        
+        var currentRepo: Repository? {
+            do {
+                return try repositories.value()[indexPath.row]
+            } catch {
+                return nil
+            }
+        }
+        
+        cell.repository = currentRepo
         
         return cell
     }
